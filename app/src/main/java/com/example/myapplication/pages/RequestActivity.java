c0acb2fca48acabd04d42a8da2dee0fde4f10d7f
anselmo228 <anselmo228@naver.com>
2023-05-28 03:28:47 +0900

    건희하기 디비 서버 전송 및 디자인 몇가지

--- app/src/main/java/com/example/myapplication/pages/RequestActivity.java
+++ app/src/main/java/com/example/myapplication/pages/RequestActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication.pages;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class RequestActivity extends AppCompatActivity {
 
     private EditText idEditText;
@@ -21,6 +32,7 @@ public class RequestActivity extends AppCompatActivity {
     private Button btnGuest;
     TextView userIdTextView;
     ImageButton back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +42,9 @@ public class RequestActivity extends AppCompatActivity {
         password = info.getStringExtra("password");
         responseText = info.getStringExtra("responseText");
 
-        // 세션 정보를 사용하는 코드 작성
-        // 예: TextView에 사용자 ID 표시
         userIdTextView = findViewById(R.id.user_id_textview);
         userIdTextView.setText("ID: " + id);
-        //뒤로가기 버튼
+
         back = findViewById(R.id.image_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,20 +59,97 @@ public class RequestActivity extends AppCompatActivity {
         btnGuest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if(id == "guest"){
-                    Toast.makeText(RequestActivity.this, "비회원으로는 리뷰를 남길 수 없습니다. 로그인 후 진행해주세요" , Toast.LENGTH_SHORT).show();
-                    onBackPressed();
-                    finish();
+                String inputText = idEditText.getText().toString();
+                String urlString = null;
+                try {
+                    urlString = "https://mobile.gach0n.com/check_session.php?session_id=" + URLEncoder.encode(responseText, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
                 }
-                else {
-                    String inputText = idEditText.getText().toString();
-                    Toast.makeText(RequestActivity.this, "정상적으로 처리되었습니다. 소중한 의견 감사합니다.", Toast.LENGTH_SHORT).show();
-
-                    onBackPressed();
-                    finish();
-                }
+                new SessionCheckTask(inputText).execute(urlString);
             }
         });
+    }
+
+    private class SessionCheckTask extends AsyncTask<String, Void, String> {
+        String content;
+
+        public SessionCheckTask(String content) {
+            this.content = content;
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoOutput(false);
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder responseBuilder = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        responseBuilder.append(line);
+                    }
+                    return responseBuilder.toString();
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            if (response != null && !response.equals("wrong session") && !response.equals("session expired")) {
+                new SuggestionTask().execute(content);
+            } else {
+                Toast.makeText(RequestActivity.this, "세션 확인 실패: " + response, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class SuggestionTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String urlString = "https://mobile.gach0n.com/suggestion.php?session_id=" + URLEncoder.encode(responseText, "UTF-8") + "&content=" + URLEncoder.encode(params[0], "UTF-8");
+                URL url = new URL(urlString);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoOutput(false);
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder responseBuilder = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        responseBuilder.append(line);
+                    }
+                    return responseBuilder.toString();
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(RequestActivity.this, result, Toast.LENGTH_SHORT).show();
+            onBackPressed();
+            finish();
+        }
     }
 }
